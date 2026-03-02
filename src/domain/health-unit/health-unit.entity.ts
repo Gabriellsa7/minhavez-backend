1a035fe7e72a605a362b0017b2081a71689bf171
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-02 15:34:26 -0300

    feat: create health unit entity

---
+++ src/domain/health-unit/health-unit.entity.ts
@@ -0,0 +1,24 @@
+import { IHealthUnit } from '../user/interfaces/health-unit.interface';
+
+export class HealthUnit implements IHealthUnit {
+  _id: string;
+
+  name: string;
+
+  address: string;
+
+  phone: string;
+
+  email: string;
+
+  createdAt: Date;
+
+  constructor(data: IHealthUnit) {
+    this._id = data._id;
+    this.name = data.name;
+    this.address = data.address;
+    this.phone = data.phone;
+    this.email = data.email;
+    this.createdAt = data.createdAt;
+  }
+}
