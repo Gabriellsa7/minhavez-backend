5946998ba09e60836306d52f48c5ce7d00206d5f
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-02 16:39:47 -0300

    feat: add the health unit controller factory

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,14 @@ const OPEN_API_SPEC_FILE_LOCATION = path.resolve(
 );
 
 import 'dotenv/config';
+import { HealthUnitControllerFactory } from './infrastructure/config/factories/health-unit/health-unit.controller.factory';
 
 const app = new Server({
   port: Number(process.env.PORT) || 3000,
-  controllers: [UserControllerFactory.create()],
+  controllers: [
+    UserControllerFactory.create(),
+    HealthUnitControllerFactory.create(),
+  ],
   databaseURI: process.env.DATABASE_URI,
   apiSpecLocation: OPEN_API_SPEC_FILE_LOCATION,
 });
