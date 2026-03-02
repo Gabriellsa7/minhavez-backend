f826f7c4a28e9e156a972d8b0e0237c4525fb196
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-02 15:20:10 -0300

    feat: create health unit service interface

---
+++ src/domain/user/interfaces/health-unit.service.interface.ts
@@ -0,0 +1,22 @@
+import {
+  IHealthUnitRepository,
+  IParamsCreateHealthUnit,
+  IParamsUpdateHealthUnit,
+} from '../repository/health-unit.repository.interface';
+import { IHealthUnit } from './health-unit.interface';
+
+export interface IParamsHealthUnitService {
+  healthRepository: IHealthUnitRepository;
+}
+
+export interface IHealthUnitService {
+  createHealthUnit(params: IParamsCreateHealthUnit): Promise<IHealthUnit>;
+  updateHealthUnitById(
+    id: string,
+    params: IParamsUpdateHealthUnit,
+  ): Promise<IHealthUnit | null>;
+  deleteHealthUnitById(id: string): Promise<IHealthUnit | null>;
+  getHealthUnitByEmail(email: string): Promise<IHealthUnit | null>;
+  getHealthUnitById(id: string): Promise<IHealthUnit | null>;
+  listHealthUnits(filter: Partial<IHealthUnit>): Promise<IHealthUnit[]>;
+}
