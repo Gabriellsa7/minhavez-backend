f45788a7b8a125e522861daa1ff86ab1b33d8d27
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-04 14:34:46 -0300

    feat: create patient schema

---
+++ src/infrastructure/db/mongo/schema/patient.schema.ts
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import { EPatientPriority } from '../../../../domain/patient/interfaces/patient.interface';
+import { Types } from 'mongoose';
+
+export const patientSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    cpf: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'Invalid CPF format'],
+    },
+
+    birthDate: {
+      type: String,
+      required: true,
+    },
+
+    priority: {
+      type: String,
+      enum: Object.values(EPatientPriority),
+      default: EPatientPriority.LOW,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    _id: true,
+    timestamps: true,
+  },
+);
+
+export interface IPatientSchema {
+  userId: Types.ObjectId;
+  cpf: string;
+  birthDate: string;
+  priority: EPatientPriority;
+  phone: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
