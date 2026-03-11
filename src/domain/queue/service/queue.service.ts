15ad6cea59617b5ee44f768590fe18664570e99d
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-11 16:40:56 -0300

    feat: add verification to allow just one opened queue for professional at the same time

--- src/domain/queue/service/queue.service.ts
+++ src/domain/queue/service/queue.service.ts
@@ -1,4 +1,4 @@
-import { IQueue } from '../interfaces/queue.interface';
+import { EQueueStatus, IQueue } from '../interfaces/queue.interface';
 import { IQueueService } from '../interfaces/queue.service.interface';
 import {
   IParamsCreateQueue,
@@ -15,17 +15,16 @@ export class QueueService implements IQueueService {
 
   async createQueue(params: IParamsCreateQueue): Promise<IQueue> {
     try {
-      //add this method to find an open queue
-      // if (params.status === 'OPEN') {
-      //   const existingOpenQueue = await this.queueRepository.findOpenQueue(
-      //     params.professionalId,
-      //     params.healthUnitId,
-      //   );
+      if (params.status === 'OPEN') {
+        const existingOpenQueue = await this.queueRepository.listQueues({
+          professionalId: params.professionalId,
+          status: EQueueStatus.OPEN,
+        });
 
-      //   if (existingOpenQueue) {
-      //     throw new Error('Already has an open queue');
-      //   }
-      // }
+        if (existingOpenQueue.length > 0) {
+          throw new Error('Already has an open queue');
+        }
+      }
       return await this.queueRepository.createQueue(params);
     } catch (error) {
       throw new Error(`Error creating queue: ${(error as Error).message}`);
