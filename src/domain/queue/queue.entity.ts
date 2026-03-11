ceb032f3b687623795cb8796457b04436d64a90e
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-11 13:28:00 -0300

    feat: create queue entity

---
+++ src/domain/queue/queue.entity.ts
@@ -0,0 +1,33 @@
+import { EQueueStatus, EQueueType, IQueue } from './interfaces/queue.interface';
+
+export class Queue implements IQueue {
+  _id: string;
+
+  professionalId: string;
+
+  healthUnitId: string;
+
+  status: EQueueStatus;
+
+  type: EQueueType;
+
+  openedAt?: Date;
+
+  closedAt?: Date;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  constructor(data: Queue) {
+    this._id = data._id;
+    this.professionalId = data.professionalId;
+    this.healthUnitId = data.healthUnitId;
+    this.status = data.status;
+    this.type = data.type;
+    this.openedAt = data.openedAt ? new Date(data.openedAt) : undefined;
+    this.closedAt = data.closedAt ? new Date(data.closedAt) : undefined;
+    this.createdAt = new Date(data.createdAt);
+    this.updatedAt = new Date(data.updatedAt);
+  }
+}
