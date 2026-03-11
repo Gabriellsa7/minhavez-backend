9bad4fe8f3ffd3431f11a22cc2602ad2a621060d
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-11 13:30:28 -0300

    feat: create queue service

---
+++ src/domain/queue/service/queue.service.ts
@@ -0,0 +1,81 @@
+import { IQueue } from '../interfaces/queue.interface';
+import { IQueueService } from '../interfaces/queue.service.interface';
+import {
+  IParamsCreateQueue,
+  IParamsUpdateQueue,
+  IQueueRepository,
+} from '../repository/queue.repository.interface';
+
+export class QueueService implements IQueueService {
+  private queueRepository: IQueueRepository;
+
+  constructor(params: { queueRepository: IQueueRepository }) {
+    this.queueRepository = params.queueRepository;
+  }
+
+  async createQueue(params: IParamsCreateQueue): Promise<IQueue> {
+    try {
+      return await this.queueRepository.createQueue(params);
+    } catch (error) {
+      throw new Error(`Error creating queue: ${(error as Error).message}`);
+    }
+  }
+
+  async getQueueById(id: string): Promise<IQueue | null> {
+    try {
+      const queue = await this.queueRepository.getQueueById(id);
+
+      if (!queue) {
+        throw new Error('Queue not found');
+      }
+
+      return queue;
+    } catch (error) {
+      throw new Error(
+        `Error retrieving queue by ID: ${(error as Error).message}`,
+      );
+    }
+  }
+
+  async updateQueueById(
+    _id: string,
+    params: IParamsUpdateQueue,
+  ): Promise<IQueue | null> {
+    try {
+      const updatedQueue = await this.queueRepository.updateQueueById(
+        _id,
+        params,
+      );
+
+      if (!updatedQueue) {
+        throw new Error('Queue not found');
+      }
+
+      return updatedQueue;
+    } catch (error) {
+      throw new Error(`Error updating queue: ${(error as Error).message}`);
+    }
+  }
+
+  async deleteQueueById(id: string): Promise<IQueue | null> {
+    try {
+      const deletedQueue = await this.queueRepository.deleteQueueById(id);
+
+      if (!deletedQueue) {
+        throw new Error('Queue not found');
+      }
+
+      return deletedQueue;
+    } catch (error) {
+      throw new Error(`Error deleting queue: ${(error as Error).message}`);
+    }
+  }
+
+  async listQueues(filter: Partial<IQueue>): Promise<IQueue[]> {
+    try {
+      return await this.queueRepository.listQueues(filter);
+    } catch (error) {
+      throw new Error(`Error listing queues: ${(error as Error).message}`);
+    }
+  }
+}
