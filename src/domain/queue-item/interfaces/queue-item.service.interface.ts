2ee7d25d1c9108bafa1247bddddd7a706cd9cb4d
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-03-14 09:33:15 -0300

    feat(queue-item): add queue item service interface

---
+++ src/domain/queue-item/interfaces/queue-item.service.interface.ts
@@ -0,0 +1,23 @@
+import {
+  IParamsCreateQueueItem,
+  IParamsUpdateQueueItem,
+  IQueueItemRepository,
+} from '../repository/queue-item.repository.interface';
+import { IQueueItem } from './queue-item.interface';
+
+export interface IParamsQueueItemService {
+  queueItemRepository: IQueueItemRepository;
+}
+
+export interface IQueueItemService {
+  createQueueItem(params: IParamsCreateQueueItem): Promise<IQueueItem>;
+  getQueueItemById(_id: string): Promise<IQueueItem | null>;
+  getQueueItemByPatientId(patientId: string): Promise<IQueueItem | null>;
+  getQueueItemByQueueId(queueId: string): Promise<IQueueItem | null>;
+  updateQueueItemById(
+    _id: string,
+    params: IParamsUpdateQueueItem,
+  ): Promise<IQueueItem | null>;
+  deleteQueueItemById(_id: string): Promise<IQueueItem | null>;
+  listQueueItem(filter: Partial<IQueueItem>): Promise<IQueueItem[]>;
+}
