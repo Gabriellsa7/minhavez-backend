b2aa3a40debfe1f89f60dc3afa18d3f208f8d341
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-31 15:07:47 -0300

    feat: inject queueItemRepository in queue service factory

--- src/infrastructure/config/factories/queue/queue.service.factory.ts
+++ src/infrastructure/config/factories/queue/queue.service.factory.ts
@@ -1,12 +1,15 @@
 import { QueueService } from '../../../../domain/queue/service/queue.service';
 import { QueueRepository } from '../../../repository/queue/queue.repository';
+import { QueueItemRepository } from '../../../repository/queue-item/queue-item.repository';
 
 export class QueueServiceFactory {
   static create() {
-    const repoRead = new QueueRepository();
+    const queueRepo = new QueueRepository();
+    const queueItemRepo = new QueueItemRepository();
 
     return new QueueService({
-      queueRepository: repoRead,
+      queueRepository: queueRepo,
+      queueItemRepository: queueItemRepo,
     });
   }
 }
