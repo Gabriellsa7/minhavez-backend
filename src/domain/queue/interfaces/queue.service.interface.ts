79c38dc5525c36c6ab54d39140a1011140a74084
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-31 15:07:38 -0300

    feat: add queueItemRepository dependency to queue service interface

--- src/domain/queue/interfaces/queue.service.interface.ts
+++ src/domain/queue/interfaces/queue.service.interface.ts
@@ -3,15 +3,18 @@ import {
   IParamsUpdateQueue,
   IQueueRepository,
 } from '../repository/queue.repository.interface';
+import { IQueueItemRepository } from '../../queue-item/repository/queue-item.repository.interface';
 import { IQueue } from './queue.interface';
 
 export interface IParamsService {
   queueRepository: IQueueRepository;
+  queueItemRepository: IQueueItemRepository;
 }
 
 export interface IQueueService {
   createQueue(params: IParamsCreateQueue): Promise<IQueue>;
   getQueueById(_id: string): Promise<IQueue | null>;
+  getQueuesByPatientId(patientId: string): Promise<IQueue[]>;
   updateQueueById(
     _id: string,
     params: IParamsUpdateQueue,
